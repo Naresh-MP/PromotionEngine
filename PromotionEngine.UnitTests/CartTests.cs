2752be1bb528312d9a7cb195aa05ac7efbe50814
Naresh M <naresh.m@outlook.com>
2021-09-23 09:44:17 +0100

    More tests to build the cart

--- PromotionEngine.UnitTests/CartTests.cs
+++ PromotionEngine.UnitTests/CartTests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System;
+using System.Linq;
 
 namespace PromotionEngine.UnitTests
 {
@@ -16,8 +17,58 @@ namespace PromotionEngine.UnitTests
             Cart cart = new Cart();
             var stockUnit = new StockUnit { Id = 'A', Price = 50.00 };
 
-            cart.AddItemsToCart(stockUnit);
+            cart.AddItemsToCart(stockUnit,1);
             Assert.AreEqual(1, cart.CartItems.Count);
+        }
+
+        [Test]
+        public void AddItemsToCart_Should_Add_Multiple_Items_To_Cart()
+        {
+            Cart cart = new Cart();
+            var stockUnitA = new StockUnit { Id = 'A', Price = 50.00 };            
+            var stockUnitB = new StockUnit { Id = 'B', Price = 45.00 };
+
+            cart.AddItemsToCart(stockUnitA, 2);
+            cart.AddItemsToCart(stockUnitB, 1);
+            Assert.AreEqual(3, cart.CartItems.Count);
+        }
+
+        [Test]
+        
+        public void GetItemsFromCart_Should_Return_VolumeOfCart_for_singleSKU()
+        {
+            Cart cart = new Cart();
+            var stockUnit = new StockUnit { Id = 'A', Price = 50.00 };
+            cart.AddItemsToCart(stockUnit, 1);
+
+            var actual = cart.GetItemsFromCart(stockUnit.Id).Count();
+
+            Assert.AreEqual(1, actual);
+        }
+
+        [Test]
+        public void ClearCart_Should_Delete_ExistingItems()
+        {
+            Cart cart = new Cart();
+            var stockUnit = new StockUnit { Id = 'A', Price = 50.00 };
+
+            cart.AddItemsToCart(stockUnit, 1);
+            cart.ClearCart();
+
+            Assert.IsEmpty(cart.CartItems);
+        }
+
+        [Test]
+        public void GetItemsFromCart_Should_Return_VolumeOfCart_for_MultileSKU()
+        {
+            Cart cart = new Cart();
+            var stockUnitA = new StockUnit { Id = 'A', Price = 50.00 };
+            var stockUnitB = new StockUnit { Id = 'B', Price = 45.00 };
+
+            cart.AddItemsToCart(stockUnitA, 3);
+            cart.AddItemsToCart(stockUnitB, 2);
+
+            Assert.AreEqual(5, cart.CartItems.Count);
         }
     }
 }
--- PromotionEngine/Models/Cart.cs
+++ PromotionEngine/Models/Cart.cs
@@ -15,9 +15,27 @@ namespace PromotionEngine
             CartItems = new List<StockUnit>();
         }
 
-        public void AddItemsToCart(StockUnit stockUnit)
+        public void AddItemsToCart(StockUnit stockUnit, int quantity)
         {
-            CartItems.Add(stockUnit);
+            for (int i = 0; i < quantity; i++)
+            {
+                CartItems.Add(stockUnit);
+            }
+        }
+
+        public IEnumerable<StockUnit> GetItemsFromCart(char sKUId)
+        {
+            return CartItems.Where(item => item.Id == sKUId);
+        }
+
+        public void ClearCart()
+        {
+            CartItems.Clear();
+        }
+
+        public double GetTotalCartValue()
+        {
+            return CartItems.Sum(item => item.Price);
         }
     }
 }
